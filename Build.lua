6b2824468646a8e85c84f05565db44134f865837
Ibowx <ibowxstudio@gmail.com>
2025-04-20 01:09:11 +0200

    Add files via upload

---
+++ Build.lua
@@ -0,0 +1,50 @@
+-- premake5.lua
+workspace "PROJECTGENERATOR"
+   architecture "x64"
+   configurations { "Debug", "Release", "Dist" }
+   startproject "PROJECTGENERATOR"
+
+-- Workspace-wide build options for MSVC
+   filter "system:windows"
+      buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus" }
+
+OutputDir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}"
+
+-- Project Properties
+project "PROJECTGENERATOR"
+   kind "ConsoleApp"
+   language "C++"
+   cppdialect "C++20"
+   targetdir "Binaries/%{cfg.buildcfg}"
+   staticruntime "off"
+
+   files { "Source/**.h", "Source/**.cpp" }
+
+   includedirs
+   {
+      "Source"
+   }
+
+   targetdir ("Binaries/" .. OutputDir .. "/%{prj.name}")
+   objdir ("Binaries/Intermediates/" .. OutputDir .. "/%{prj.name}")
+
+   filter "system:windows"
+       systemversion "latest"
+       defines { "WINDOWS" }
+
+   filter "configurations:Debug"
+       defines { "DEBUG" }
+       runtime "Debug"
+       symbols "On"
+
+   filter "configurations:Release"
+       defines { "RELEASE" }
+       runtime "Release"
+       optimize "On"
+       symbols "On"
+
+   filter "configurations:Dist"
+       defines { "DIST" }
+       runtime "Release"
+       optimize "On"
+       symbols "Off"
